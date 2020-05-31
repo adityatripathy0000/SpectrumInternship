5bc5df42f28f8958d055b984d961dd53fe85feb1
Aditya Prasad Tripathy <43117262+adityatripathy0000@users.noreply.github.com>
2020-05-31 23:45:12 +0530

    Update FinalTask.py

--- Final_Task/FinalTask.py
+++ Final_Task/FinalTask.py
@@ -87,6 +87,15 @@ SL = 0.05
 X_opt = X[:, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]]
 X_Modeled = backwardElimination(X_opt, SL)
 
+'''
+So, important factors are:
+x1 is age,
+x2 is famrel,
+x3 is absences,
+x4 is G1,
+x5 is G2.
+'''
+
 #Visualisation Insights
 
 #Bar-plot that provide insight how the 'sex' of the students relate their 'final_grade'
